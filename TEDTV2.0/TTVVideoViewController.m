07392889dfbe9664a892b45b1abe20148a749818
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2014-10-13 14:56:41 +0900

    TED動画の連続再生が出来る

--- TEDTV2.0/TTVVideoViewController.m
+++ TEDTV2.0/TTVVideoViewController.m
@@ -14,11 +14,17 @@
 #import "DDXMLElementAdditions.h"
 #import "DDXMLElement+TTVAdditions.h"
 
-@interface TTVVideoViewController ()
+@interface TTVVideoViewController () <YTPlayerViewDelegate>
 
 @property (nonatomic, weak) IBOutlet YTPlayerView *playerView;
 
+@property (nonatomic, copy) NSArray *videoData;
+
 - (NSString *)videoID;
+
+- (void)loadVideoData;
+
+- (void)playNextVideo;
 
 @end
 
@@ -28,15 +34,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+	[self loadVideoData];
+	
+	self.playerView.delegate = self;
 }
 
 - (void)viewWillAppear:(BOOL)animated {
 	[super viewWillAppear:animated];
+		
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+	[super viewDidAppear:animated];
 	
-	NSLog(@"%@", self.content);
-	
-	[self.playerView loadWithVideoId:self.videoID];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -55,6 +66,9 @@
 	
 	self.title = [content childValueForElementName:@"title"];
 	self.navigationItem.title = self.title;
+	
+	[self view];
+	[self.playerView loadWithVideoId:self.videoID];
 }
 
 - (NSString *)videoID {
@@ -63,6 +77,63 @@
 	NSArray *components = [guid componentsSeparatedByString:@":"];
 	
 	return components.lastObject;
+}
+
+#pragma mark - Loading
+- (void)loadVideoData {
+	
+	NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+	NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+	
+	
+	NSURL *URL = [NSURL URLWithString:@"http://www.youtube.com/rss/user/TEDtalksDirector/videos.rss"];
+	NSURLSessionTask *task = [session dataTaskWithURL:URL completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+		
+		//			NSLog(@"request finished");
+		//			NSLog(@"%@", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
+		
+		DDXMLDocument *document = [[DDXMLDocument alloc] initWithData:data options:DDXMLDocumentXMLKind error:NULL];
+		DDXMLElement *rootElement = document.rootElement;
+		
+		self.videoData = [[rootElement elementForName:@"channel"] elementsForName:@"item"];
+		[self performSelectorOnMainThread:@selector(setContent:) withObject:self.videoData[0] waitUntilDone:NO];
+		
+		//			NSLog(@"%@", json);
+		
+	}];
+	
+	[task resume];
+}
+
+#pragma mark - Behavior
+- (void)playNextVideo {
+
+	NSInteger currentIndex = [self.videoData indexOfObject:self.content];
+	NSInteger count = self.videoData.count;
+	NSInteger index = (currentIndex + count + 1) % count;
+	
+	self.content = self.videoData[index];
+}
+
+#pragma mark - YTPlayerViewDelegate
+- (void)playerViewDidBecomeReady:(YTPlayerView *)playerView {
+	
+//	NSLog(@"ready");
+	[self.playerView playVideo];
+}
+
+- (void)playerView:(YTPlayerView *)playerView didChangeToState:(YTPlayerState)state {
+	
+//	NSLog(@"state: %d", state);
+	
+	switch (state) {
+		case kYTPlayerStateEnded:
+			[self playNextVideo];
+			break;
+			
+		default:
+			break;
+	}
 }
 
 @end
